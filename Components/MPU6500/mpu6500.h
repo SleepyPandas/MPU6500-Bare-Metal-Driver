29a30c33406e74f99f6c107b6feb4585f6b8cc00
SleepyPandas <tommyrobotics1@gmail.com>
2026-01-27 19:55:19 -0500

    Corrected Register Values on ACC_FS and I2C ADDR auto Shifted Left 1 <<

--- Components/MPU6500/mpu6500.h
+++ Components/MPU6500/mpu6500.h
@@ -8,24 +8,25 @@ extern "C" {
 #include "stm32h5xx_hal.h"
 #include <stdint.h>
 
-#define MPU6500_I2C_ADDR 0x68U
+#define MPU6500_I2C_ADDR (0x68U << 1) // Left Shifted 1 For I2C Address Spacing
 #define MPU6500_REG_WHO_AM_I 0x75U
 #define MPU6500_REG_PWR_MGMT_1 0x6BU
-#define MPU6500_REG_ACCEL_CONFIG 0x1CU 
+#define MPU6500_REG_ACCEL_CONFIG 0x1CU
 #define MPU6500_REG_GYRO_CONFIG 0x1BU
 
 typedef enum {
-    MPU6500_ACC_FS_2G = 0x00,
-    MPU6500_ACC_FS_4G = 0x01,
-    MPU6500_ACC_FS_8G = 0x10,
-    MPU6500_ACC_FS_16G = 0x18,
-}SetAccelRange;
+  MPU6500_ACC_FS_2G = 0x00,
+  MPU6500_ACC_FS_4G = 0x08,  // 0000 1000
+  MPU6500_ACC_FS_8G = 0x10,  // 0001 0000
+  MPU6500_ACC_FS_16G = 0x18, // 0001 1000
+} AccelRange;
 
 HAL_StatusTypeDef MPU6500_Init(I2C_HandleTypeDef *hi2c, uint8_t *who_am_i);
 
-HAL_StatusTypeDef MPU6500_SetAccelRange(I2C_HandleTypeDef *hi2c, uint8_t range);
+HAL_StatusTypeDef MPU6500_SetAccelRange(I2C_HandleTypeDef *hi2c, AccelRange range);
 
-HAL_StatusTypeDef MPU6500_SetRotationRange(I2C_HandleTypeDef *hi2c, uint8_t range);
+HAL_StatusTypeDef MPU6500_SetRotationRange(I2C_HandleTypeDef *hi2c,
+                                           uint8_t range);
 
 #ifdef __cplusplus
 }
