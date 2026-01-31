60dfb2f76c5f43d05a4be8d1d2069ce1cf2dea56
SleepyPandas <tommyrobotics1@gmail.com>
2026-01-30 19:28:46 -0500

    accelerometer and gyroscope measurement registers

--- Components/MPU6500/mpu6500.h
+++ Components/MPU6500/mpu6500.h
@@ -12,7 +12,11 @@ extern "C" {
 #define MPU6500_REG_WHO_AM_I 0x75U
 #define MPU6500_REG_PWR_MGMT_1 0x6BU
 #define MPU6500_REG_ACCEL_CONFIG 0x1CU
+#define MPU6500_REG_ACCEL_MEASURE                                              \
+  0x3BU //  Registers 59 to 64 – Accelerometer Measurements
 #define MPU6500_REG_GYRO_CONFIG 0x1BU
+#define MPU6500_REG_GYRO_MEASURE                                               \
+  0x43U // Registers 67 to 72 – Gyroscope Measurements
 
 typedef enum {
   MPU6500_ACC_FS_2G = 0x00,
@@ -42,7 +46,7 @@ HAL_StatusTypeDef MPU6500_SetRotationRange(I2C_HandleTypeDef *hi2c,
                                            uint8_t range);
 
 HAL_StatusTypeDef MPU6500_Read_Gyro_Data(I2C_HandleTypeDef *hi2c,
-                                    MPU6500_Gyro_Data *Gyro_Data);
+                                         MPU6500_Gyro_Data *Gyro_Data);
 
 HAL_StatusTypeDef MPU6500_Read_Accel_Data(I2C_HandleTypeDef *hi2c,
                                           MPU6500_Accel_Data *Accel_Data);
